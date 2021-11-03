094dfc052f0467a4bdac40bbb345180412d77938
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-04 01:43:49 +0300

    ...

--- src/BasicOperations.java
+++ src/BasicOperations.java
@@ -1,12 +1,27 @@
 public class BasicOperations {
     public static void main(String[] args) {
-        int result = 1+ 2; // 2 + 1 =3
-        System.out.println(" 2 + 1 = " +  result ) ;
+
+        int result = 1 + 2; // 1 + 2 = 3
+
+        System.out.println("1 + 2 = " +  result ) ;
 
         int previousResult = result ;
-        System.out.println("previousResult " + previousResult ) ;
-        result = result + 1 ; // 3 + 1
-        System.out.println("result + 1 = " + result ) ;
+
+        System.out.println("previousResult = " + previousResult ) ;
+
+        result = result - 1 ; // 3 - 1 = 2
+
+        System.out.println("3 - 1 = " + result ) ;
+
+        System.out.println("previousResult = " + previousResult ) ;
+
+        result = result * 5;    // 2 * 5 = 10
+
+        System.out.println("2 * 5 = " + result);
+
+        System.out.println("previousResult = " + previousResult ) ;
+
+
 
     }
 }
