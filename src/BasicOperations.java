4515915e13968dcf77fca36de6c03e47e969e061
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-04 02:14:58 +0300

    ...

--- src/BasicOperations.java
+++ src/BasicOperations.java
@@ -21,7 +21,30 @@ public class BasicOperations {
 
         System.out.println("previousResult = " + previousResult ) ;
 
+        result = result / 10;   // 10 / 10 = 1
 
+        System.out.println("10 / 10 = " + result);
+
+            // result = result + 1
+
+        result++ ;  // 1 + 1 = 2
+
+        System.out.println("1 + 1 = " + result);
+
+        result-- ;  //2 - 1 = 1
+
+        System.out.println("2 - 1 = " + result) ;
+
+            // result = result + 2
+        result += 2 ;   //1 + 2 = 3
+
+        System.out.println("1 + 2 = " + result);
+
+            // result = result * 10
+
+        result *= 10 ;  //3 * 10 = 30
+
+        System.out.println("3 * 10 = " + result);
 
     }
 }
