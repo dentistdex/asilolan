b1711b034b5f590981bc1559fa37430c81363639
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-03 21:19:52 +0300

    burdan devam edicez

---
+++ src/Hello.java
@@ -0,0 +1,25 @@
+public class Hello {
+
+        public static void main(String[] args) {
+                System.out.println("Hello, Tuncay");
+                int myfirstnumber = 5 + 25 ;
+                int mysecondenumber = 75 ;
+                int mytotal = (myfirstnumber) + (mysecondenumber) ;
+                System.out.println("toplam" + (mytotal )) ;
+
+
+                int myPoundValue = 1 ;
+                double myKilogramValue = 0.45359237d ;
+
+                System.out.println("5 pound = " + (5 * myPoundValue * myKilogramValue) + " kilogram" );
+
+
+                int result = 1 + 2 ; // 2 + 1 = 3
+
+                System.out.println ("2 + 1 = " + result) ;
+
+
+
+
+        }
+}
