a08cf253886fe51c2e3d0e1c4aa45d709e9a5724
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-14 00:49:27 +0300

    ...

--- src/MethodOverload.java
+++ src/MethodOverload.java
@@ -4,6 +4,8 @@ public class MethodOverload {
         double calcFeetAndInchesToCentimeters = calcFeetAndInchesToCentimeters(6.0);
         calcFeetAndInchesToCentimeters(6,0);
         calcFeetAndInchesToCentimeters(157);
+        getDurationString(61,0);
+        getDurationString(7800);
     }
 
     public static double calcFeetAndInchesToCentimeters ( double feet, double inches){
@@ -28,4 +30,27 @@ public class MethodOverload {
 
     }
 
+    public static int getDurationString (int minutes , int seconds){
+        if (minutes < 0 || (seconds < 0 || seconds > 59)) {
+            System.out.println("Invalid Value");
+            return -1;
+        }else {
+            int hours = minutes / 60;
+            int remainderMinutes = minutes % 60;
+            System.out.println(hours + "h " + remainderMinutes + "m " + seconds + "s");
+            return seconds;
+        }
+    }
+    public static int getDurationString ( int seconds){
+        if (seconds < 0 ){
+            System.out.println("Invalid Value");
+            return -1;
+        }else {
+            int minutes = seconds / 60;
+            int remainderSeconds = seconds % 60;
+            System.out.println(seconds + " equals to = " + minutes + " minutes and " + remainderSeconds + " seconds");
+            return getDurationString(minutes,remainderSeconds) ;
+        }
+
+    }
 }
